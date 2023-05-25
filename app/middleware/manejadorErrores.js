a0321a1fbf409fe053c747df521464e213da0075
Willy Corzo <ing.willy.corzo@gmail.com>
2023-05-25 07:55:30 -0500

    Implementado el Ingreso con Token

--- app/middleware/manejadorErrores.js
+++ app/middleware/manejadorErrores.js
@@ -5,8 +5,6 @@ import colors from 'colors'
 // Importacion de registroEventos
 import { registroEventos } from './registroEventos.js'
 
-import { constants } from '../../configuracion/constants.js'
-
 // Funcion para crear y guardar un Error
 export const ManejadorErrores = (err, req, res, next) => {
   registroEventos(
@@ -18,32 +16,32 @@ export const ManejadorErrores = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500
 
   switch (statusCode) {
-    case constants.VALIDATION_ERROR:
+    case 400:
       res.json({
         title: 'Validation Failed',
         message: err.message,
         stackTrace: err.stack
       })
       break
-    case constants.NOT_FOUND:
+    case 404:
       res.json({
         title: 'Not Found',
         message: err.message,
         stackTrace: err.stack
       })
-    case constants.UNATHORIZED:
+    case 401:
       res.json({
         title: 'Unauthorized',
         message: err.message,
         stackTrace: err.stack
       })
-    case constants.FORBIDDEN:
+    case 403:
       res.json({
         title: 'Forbidden',
         message: err.message,
         stackTrace: err.stack
       })
-    case constants.SERVER_ERROR:
+    case 500:
       res.json({
         title: 'Server Error',
         message: err.message,
